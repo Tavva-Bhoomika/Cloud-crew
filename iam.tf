c1d7d162c8bdb55501b1ab789472a1deef8d1026
Tavva-Bhoomika <bhoomikatavva03@gmail.com>
2024-09-12 13:04:41 +0530

    Create iam.tf

---
+++ iam.tf
@@ -0,0 +1,45 @@
+# Define the IAM role
+resource "aws_iam_role" "cloud-crew_role" {
+  name = "cloud-crew_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+      },
+    ],
+  })
+}
+
+# Attach Amazon EC2 Full Access policy to the IAM role
+resource "aws_iam_role_policy_attachment" "ec2_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+  role     = aws_iam_role.cloud-crew_role.name
+}
+
+# Attach Amazon RDS Full Access policy to the IAM role
+resource "aws_iam_role_policy_attachment" "rds_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonRDSFullAccess"
+  role     = aws_iam_role.cloud-crew_role.name
+}
+
+# Attach CloudWatch Full Access policy to the IAM role
+resource "aws_iam_role_policy_attachment" "cloudwatch_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchFullAccess"
+  role     = aws_iam_role.cloud-crew_role.name
+}
+
+# Attach CloudFront Full Access policy to the IAM role
+resource "aws_iam_role_policy_attachment" "cloudfront_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonCloudFrontFullAccess"
+  role     = aws_iam_role.cloud-crew_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "cloud-crew_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+  role     = aws_iam_role.cloud-crew_role.name
+}
