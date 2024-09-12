e4cb39613f7f3ed38679a22d5233485ee11db63c
Tavva-Bhoomika <bhoomikatavva03@gmail.com>
2024-09-12 12:53:05 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,10 @@
+# ec2.tf
+resource "aws_instance" "web_server" {
+  ami           = "ami-0c55b159cbfafe1f0"  # Replace with your preferred AMI ID
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.main.id
+  tags = {
+    Name = "web-server-ec2"
+  }
+  security_groups = [aws_security_group.web_sg.name]
+}
