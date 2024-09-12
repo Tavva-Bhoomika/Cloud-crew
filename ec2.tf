18cb1f0cf05c7f2d3a5b33f88d06b925a2f11a84
Tavva-Bhoomika <bhoomikatavva03@gmail.com>
2024-09-12 16:27:21 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,9 @@
-resource "aws_instance" "web_server" {
-  ami           = "ami-0888ba30fd446b771"  # Replace with your preferred AMI ID
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.main.id
-  tags = {
-    Name = "web-server-ec2"
-  }
-  security_groups = [aws_security_group.web_sg.name]
-}
+#resource "aws_instance" "web_server" {
+#  ami           = "ami-0888ba30fd446b771"  # Replace with your preferred AMI ID
+#  instance_type = "t2.micro"
+#  subnet_id     = aws_subnet.main.id
+#  tags = {
+#    Name = "web-server-ec2"
+#  }
+#  security_groups = [aws_security_group.web_sg.name]
+#}
