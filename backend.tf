7d2d214c8889ddcedeb667f225e2c62fc2457689
Tavva-Bhoomika <bhoomikatavva03@gmail.com>
2024-09-12 12:50:39 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 # backend.tf
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"
+    bucket         = "cloud-crew-terraform-state-bucket"
     key            = "terraform/state.tfstate"
     region         = "ap-south-1"
   }
