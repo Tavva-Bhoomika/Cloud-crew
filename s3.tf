59f935b4de589bd548e63e8147fa062b561d1dfb
Tavva-Bhoomika <bhoomikatavva03@gmail.com>
2024-09-12 15:45:33 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,11 +1,15 @@
 resource "aws_s3_bucket" "website" {
   bucket = "cloud-crew-static-website-bucket"
   acl    = "public-read"
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
-  }
+
   tags = {
     Name = "static-website-bucket"
   }
+}
+
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  index_document = "index.html"
+  error_document = "error.html"
 }
