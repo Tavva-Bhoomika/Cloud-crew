50b21323cbc247fc5b91b7ffe7ff4028c31c9c6b
Tavva-Bhoomika <bhoomikatavva03@gmail.com>
2024-09-12 12:58:07 +0530

    Create rds.tf

---
+++ rds.tf
@@ -0,0 +1,15 @@
+# rds.tf
+resource "aws_db_instance" "default" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "8.0.35"
+  instance_class       = "db.t3.micro"
+  name                 = "cloud-crew"
+  username             = "admin"
+  password             = "password123"
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
+  vpc_security_group_ids = [aws_security_group.db_sg.id]
+  db_subnet_group_name   = aws_db_subnet_group.default.name
+}
