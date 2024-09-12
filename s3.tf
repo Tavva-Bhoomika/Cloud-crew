9f1fb6eac019f1e4e7938ea5ddea4d3d2711c900
Tavva-Bhoomika <bhoomikatavva03@gmail.com>
2024-09-12 17:45:45 +0530

    new

--- s3.tf
+++ s3.tf
@@ -1,6 +1,5 @@
 resource "aws_s3_bucket" "website" {
   bucket = "cloud-crew-static-website-bucket"  # Ensure this name is unique
-  acl    = "public-read"  # Sets the ACL to allow public read access
 
   website {
     index_document = "index.html"
